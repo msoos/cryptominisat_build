1803802d4232c120a7d45d1b9b206c0d386d9e02
Mate Soos <soos.mate@gmail.com>
2016-02-01 00:03:24 +0000

    Build with 6 threads for normal build

--- build_norm.sh
+++ build_norm.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON ..
-make -j4
+make -j6
 make test
 cd ../tests/simp-checks/
 ./checks.py ../../build/cryptominisat4 testfiles/*
