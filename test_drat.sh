f7e04cc60b98d23f2ecd0894afa0be1926b28dd9
Mate Soos <soos.mate@gmail.com>
2016-02-29 21:45:48 +0000

    Fixing DRAT check scripts

--- test_drat.sh
+++ test_drat.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 
 ./cryptominisat4 --drat=drat_test.drat --input=drat_test.cnf --zero-exit-status
-cat mytestdrup 
+cat drat_test.drat
 ../../drat-trim/drat-trim drat_test.cnf drat_test.drat 
