96eeef9d88c6d5155059b19f13694ed4b9285f1a
Mate Soos <soos.mate@gmail.com>
2016-02-29 22:58:07 +0000

    More thorough test for prediction

--- test_predict.py
+++ test_predict.py
@@ -7,3 +7,6 @@ rm -if drat_test2.cnf.gz.sqlite
 ./cryptominisat4 --verb 0 drat_test2.cnf.gz --zero-exit-status --clid drat_out --sql 2
 ../../drat-trim2/drat-trim2 drat_test2.cnf.gz drat_out -l lemmas
 ./add_lemma_ind.py drat_test2.cnf.gz.sqlite lemmas
+./predict.py drat_test2.cnf.gz.sqlite
+dot -Tpng drat_test2.tree,dot -o tree.png
+okular tree.png
