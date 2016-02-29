e7935fa9273dbe7d00767d03f2b33a978448ac2d
Mate Soos <soos.mate@gmail.com>
2016-02-29 22:44:38 +0000

    Simplifying DRAT test

--- test_drat.sh
+++ test_drat.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-./cryptominisat4 --drat=drat_test.drat --input=drat_test.cnf --zero-exit-status
+./cryptominisat4 drat_test.cnf drat_test.drat --zero-exit-status
 cat drat_test.drat
 ../../drat-trim/drat-trim drat_test.cnf drat_test.drat 
