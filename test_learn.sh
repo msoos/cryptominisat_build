3910dc8485ef9d81ec179947c421c778150817eb
Mate Soos <soos.mate@gmail.com>
2016-02-29 22:43:45 +0000

    Better test scripts

--- test_learn.sh
+++ test_learn.sh
@@ -4,6 +4,5 @@ set -x
 
 rm -if dratout
 rm -if drat_test2.cnf.gz.sqlite
-./cryptominisat4 drat_test2.cnf.gz --zero-exit-status --clid drat_out
-../../drat-trim2/drat-trim2 drat_test2.cnf.gz drat_out
-
+./cryptominisat4 --verb 0 drat_test2.cnf.gz --zero-exit-status --clid drat_out --sql 2
+../../drat-trim2/drat-trim2 drat_test2.cnf.gz drat_out -l lemmas
