2e05d6058d0e3d47baa4c9e5947a608199243f7c
Mate Soos <soos.mate@gmail.com>
2016-03-25 01:09:45 +0000

    Use valgrind extra lib on slowdebug build

--- build_slowdebug.sh
+++ build_slowdebug.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include
-cmake -DENABLE_TESTING=ON -DSLOW_DEBUG=ON ..
+cmake -DENABLE_TESTING=ON -DSLOW_DEBUG=ON -DNOVALGRIND=OFF ..
 make -j6 VERBOSE=1
 make test
