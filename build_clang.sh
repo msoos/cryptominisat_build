42a304c0a37a6009e9c1127b80947cd730a93454
Mate Soos <soos.mate@gmail.com>
2016-04-05 00:02:29 +0100

    Build with clang too

---
+++ build_clang.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
+CXX=clang++ cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON ..
+make -j6
+make test
+cd ../tests/simp-checks/
+./checks.py ../../build/cryptominisat4 testfiles/*
+cd ../../build/
