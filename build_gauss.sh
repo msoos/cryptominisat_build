adceaa51e924620c0d888b04791897666732dfe1
Mate Soos <soos.mate@gmail.com>
2016-05-18 00:25:52 +0100

    Updating scripts to delete old binaries and adding tests when needed

--- build_gauss.sh
+++ build_gauss.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include scalmc* cusp*
 rm -rf tests
 cmake -DUSE_GAUSS=ON -DENABLE_TESTING=ON ..
 make -j6
--- build_norm.sh
+++ build_norm.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc*
 cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON ..
 make -j6
 make test
