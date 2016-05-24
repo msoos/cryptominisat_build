119f17506450f8011f5800cf7c84e51168060aa5
Mate Soos <soos.mate@gmail.com>
2016-05-24 23:13:47 +0100

    Adding more build scripts

---
+++ build_clang_scan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc*
+scan-build cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON ..
+scan-build make -j6
+make test
+cd ../tests/simp-checks/
+./checks.py ../../build/cryptominisat4 testfiles/*
+cd ../../build/
