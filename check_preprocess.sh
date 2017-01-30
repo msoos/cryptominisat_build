0f7084f030a44be8d0b57e691c0b80f1ac307621
Mate Soos <soos.mate@gmail.com>
2017-01-30 21:55:31 +0000

    Fixing preprocess check script

--- check_preprocess.sh
+++ check_preprocess.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 set -x
+set -h
 
 ../../cnf-utils/build/cnf-fuzz-biere > tmp.cnf
-./cryptominisat5 -p 1 tmp.cnf simplified.cnf
-./cryptominisat5 simplified.cnf > solution.txt
-./cryptominisat5 -p 2 solution.txt
-
+./cryptominisat5 --printsol 0 --verb 0 -p 1 tmp.cnf --zero-exit-status simplified.cnf
+./cryptominisat5 --verb 0 --zero-exit-status simplified.cnf > solution.txt
+./cryptominisat5 --printsol 0 --verb 0 -p 2 --zero-exit-status solution.txt
+lingeling --witness=0 tmp.cnf
