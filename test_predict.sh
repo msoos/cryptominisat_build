9360d5e120bb69612e0508de644cdc21776b389c
Mate Soos <soos.mate@gmail.com>
2017-02-03 21:22:14 +0000

    Testing for SQL in prediction script

--- test_predict.sh
+++ test_predict.sh
@@ -4,6 +4,11 @@ set -x
 
 rm -if dratout
 rm -if drat_test2.cnf.gz.sqlite
+status=`./cryptominisat5 --hhelp | grep sql || ret=$?`
+if [ "$ret" -ne 0 ]; then
+    echo "You must compile SQL into cryptominisat"
+    exit -1
+fi
 ./cryptominisat5 --verb 0 drat_test2.cnf.gz --zero-exit-status --clid drat_out --sql 2
 ../../drat-trim2/drat-trim2 drat_test2.cnf.gz drat_out -l lemmas
 ./add_lemma_ind.py drat_test2.cnf.gz.sqlite lemmas
