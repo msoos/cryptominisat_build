513f6bac3eef3c78a324ab73ab364e6d43bc7e89
Mate Soos <soos.mate@gmail.com>
2017-02-03 21:21:23 +0000

    For clang static scanning, there is no need to run the tests

--- build_clang_scan.sh
+++ build_clang_scan.sh
@@ -5,7 +5,3 @@ set -e
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc*
 scan-build cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON ..
 scan-build make -j6
-make test
-cd ../tests/simp-checks/
-./checks.py ../../build/cryptominisat5 testfiles/*
-cd ../../build/
