9ded042a3c8fffa9a54d8eb2b4bc27739a1d2de9
Mate Soos <soos.mate@gmail.com>
2017-02-05 20:05:12 +0000

    Don't interfere guesses with prediction generation

--- test_predict.sh
+++ test_predict.sh
@@ -11,7 +11,7 @@ if [ "$ret" -ne 0 ]; then
 fi
 
 # running CNF
-./cryptominisat5 --verb 0 drat_test2.cnf.gz --zero-exit-status --clid --sql 2 drat_out
+./cryptominisat5 --verb 0 drat_test2.cnf.gz --keepguess 0 --zero-exit-status --clid --sql 2 drat_out
 
 # getting drat
 ../../drat-trim/drat-trim2 drat_test2.cnf.gz drat_out -l lemmas
