ac96c78e0569fa9d060d17f916c00d8666cf2944
Mate Soos <soos.mate@gmail.com>
2017-04-20 01:06:34 +0100

    Adding NOSQLITE build

---
+++ build_nosqlite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc*
+cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DNOSQLITE=ON ..
+make -j6
+make test
+(
+    cd ../tests/simp-checks/
+    ./check_bve.py ../../build/cryptominisat5 testfiles/*
+)
