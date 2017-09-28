55d5b83763d25960a222381723efe2350f058e25
Mate Soos <soos.mate@gmail.com>
2017-09-28 23:35:52 +0100

    Fixing build_stats

--- build_stats.sh
+++ build_stats.sh
@@ -7,6 +7,3 @@ rm -f ../tests/cnf-files/*sqlite
 cmake -DSTATS=ON -DENABLE_TESTING=ON ..
 make -j6
 make test
-cd ../tests/simp-checks/
-./check_bve.py ../../build/cryptominisat5 testfiles/*
-cd ../../build/
