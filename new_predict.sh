c02e10bb18687d3c28bfee07034d8ed59a0c2c5b
Mate Soos <soos.mate@gmail.com>
2017-10-01 15:31:23 +0100

    Cleaner predict script

--- new_predict.sh
+++ new_predict.sh
@@ -8,31 +8,32 @@ mkdir -p ${OUTDIR}
 for i in `seq 1 6`;
 do
     FNAME="my_cnf_$i.cnf"
-    rm -if dratout
-    rm -f lemmas
+    rm -if "${OUTDIR}/dratout"
+    rm -f "${OUTDIR}/lemmas"
     rm -f "${FNAME}"
 
     # generate CNF
     if [ $((a%2)) -eq 0 ];
     then
-         ../build/tests/cnf-utils/sgen4 -unsat -n 62 -s 1 > $FNAME
+         ./tests/cnf-utils/sgen4 -unsat -n 62 -s 1 > ${OUTDIR}/${FNAME}
     fi
 
     if [ $((a%2)) -eq 1 ];
     then
-        ../../sha1-sat/build/sha1-gen --attack preimage --rounds 16 --message-bits 430 --zero --seed ${i} > "${FNAME}"
+        ./tests/sha1-sat/sha1-gen --attack preimage --rounds 16 \
+            --message-bits 430 --zero --seed ${i} > "${OUTDIR}/${FNAME}"
     fi
 
     # running CNF
     # --maxtemp 1000000000 --presimp 0 --schedsimp 0
-    ./cryptominisat5 --verb 0 --keepguess 0 --zero-exit-status --clid --sql 2 "${FNAME}" drat_out
+    ./cryptominisat5 --verb 0 --keepguess 0 --zero-exit-status --clid --sql 2 \
+        "${OUTDIR}/${FNAME}" "${OUTDIR}/drat_out" --sqlitedb "${OUTDIR}/${FNAME}.sqlite"
 
-    # getting drat
-    ../../drat-trim/drat-trim "${FNAME}" drat_out -l lemmas
+    # getting lemmas used from DRAT
+    ./tests/drat-trim/drat-trim "${FNAME}" "${OUTDIR}/drat_out" -l "${OUTDIR}/lemmas${i}"
 
     # add lemma indices that were good
-    mv "${FNAME}.sqlite" "${OUTDIR}/"
-    ./add_lemma_ind.py "${OUTDIR}/${FNAME}.sqlite" lemmas
+    ./add_lemma_ind.py "${OUTDIR}/${FNAME}.sqlite" "${OUTDIR}/lemmas${i}"
     echo "Done with $i"
 done
 
