27eb06ff65608410a12623b8f9f6800767e4d1cf
Mate Soos <soos.mate@gmail.com>
2018-02-09 00:22:26 +0000

    Fixing the fuzzer

--- build_afl.sh
+++ build_afl.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
-CC=afl-clang-fast CXX=afl-clang-fast++ cmake -DSANITIZE=ON -DENABLE_PYTHON_INTERFACE=OFF -DENABLE_TESTING=OFF -DNOZLIB=ON ..
+CC=afl-clang-fast CXX=afl-clang-fast++ cmake -DLIMITMEM=ON -DENABLE_PYTHON_INTERFACE=OFF -DENABLE_TESTING=OFF -DNOZLIB=ON ..
 make -j6
-afl-fuzz -m 500 -i afl/testcase_dir/ -o afl/finddings_dir/ ./cryptominisat5
+afl-fuzz -M master -i afl/testcase_dir/ -o afl/findings_dir/ ./cryptominisat5 --debuglib 1
