d2015f9c50a81822e8c569313319ec909cc9be2f
Mate Soos <soos.mate@gmail.com>
2018-04-17 16:02:23 +0800

    Fixing test-predict for newer drat

--- test_predict.sh
+++ test_predict.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 DIR=test_predict
-./predict_one.sh drat_test2.cnf.gz ${DIR}
+./predict_one.sh drat_test2.cnf ${DIR}
 
 dot -Tpng ${DIR}/data.sqlite.tree.dot -o tree.png
 echo "Execute:"
