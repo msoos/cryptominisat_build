f1ece5ab057e817b7a8f722bdd17cb8373bce4d8
Mate Soos <soos.mate@gmail.com>
2018-05-10 22:00:28 +0800

    Fixing predictor test

--- test_predict2.sh
+++ test_predict2.sh
@@ -4,8 +4,7 @@ set -x
 
 DIR=test_predict2
 rm -f q_query_3_L200_coli.sat.cnf.*
-./predict_one.sh q_query_3_L200_coli.sat.cnf ${DIR} 0.05
+./predict_one.sh q_query_3_L200_coli.sat.cnf ${DIR} 0.3
 
-dot -Tpng ${DIR}/data.sqlite.tree.dot -o tree.png
 echo "Execute:"
 echo "display tree.png"
