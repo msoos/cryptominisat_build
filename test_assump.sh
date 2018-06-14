2ac3e2e3cd69bb994e89c084d9c3722f3d98feac
Mate Soos <soos.mate@gmail.com>
2018-06-14 17:11:16 +0800

    Adding test assumption script

---
+++ test_assump.sh
@@ -0,0 +1,52 @@
+#/usr/bin/bash
+
+if [[ 1 -eq 1 ]]; then
+    rm -f out dr x
+    rm -f assump.*
+    ./tests/cnf-utils/sgen4 -sat -n 80 -s 10  > out
+    ./cryptominisat5 out  dr --verb 0
+    echo "-1" > assump.ass
+    ./cryptominisat5 out  --assump assump.ass  dr --verb 0
+    ./tests/drat-trim/drat-trim out dr -A assump.ass -l x -B
+    cp out out_more
+    grep -v "d" x | grep -v "^0$" | sed "s/ 0 .*/ 0/" >> out_more
+    out=$(./cryptominisat5 out_more --verb 0)
+    echo $out
+fi
+
+
+
+if [[ 1 -eq 1 ]]; then
+    rm -f out dr x
+    rm -f assump.*
+    ./tests/cnf-utils/cnf-fuzz-biere 10 > out
+    ./cryptominisat5 out  dr --verb 0
+    cat >assump.ass <<EOL
+1
+2
+-3
+-4
+5
+6
+-7
+-8
+9
+-10
+-11
+12
+13
+-14
+15
+-16
+17
+18
+-19
+-20
+EOL
+    ./cryptominisat5 out  --assump assump.ass  dr --verb 0
+    ./tests/drat-trim/drat-trim out dr -A assump.ass -l x -B
+    cp out out_more
+    grep -v "d" x | grep -v "^0$" | sed "s/ 0 .*/ 0/" >> out_more
+    out=$(./cryptominisat5 out_more --verb 0)
+    echo "$out"
+fi
