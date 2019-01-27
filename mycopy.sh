6cd3f9342da90a277fc89b9eaef5226b7460066e
Mate Soos <soos.mate@gmail.com>
2019-01-27 22:59:11 +0100

    We should also copy the prediction code to the cluster

--- mycopy.sh
+++ mycopy.sh
@@ -2,6 +2,5 @@
 
 set -x
 set -e
-scp gen_pandas.py nscc:/home/projects/11000744/matesoos/cryptominisat5-predict-main
-scp rem_data.py add_lemma_ind.py nscc:/home/projects/11000744/matesoos/cryptominisat5-predict-main
+scp rem_data.py add_lemma_ind.py combine_dats.py predict.py gen_pandas.py nscc:/home/projects/11000744/matesoos/cryptominisat5-predict-main
 scp ../scripts/output_parser/solvetimes_from_output.sh nscc://home/projects/11000744/matesoos/cryptominisat5-main/
