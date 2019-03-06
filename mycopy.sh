1a7651f28003274ae505c64751ec4e06c2a98a1f
Mate Soos <soos.mate@gmail.com>
2019-03-06 22:19:34 +0100

    Updating copy script to cluster

--- mycopy.sh
+++ mycopy.sh
@@ -3,4 +3,4 @@
 set -x
 set -e
 scp rem_data.py add_lemma_ind.py combine_dats.py predict.py gen_pandas.py nscc:/home/projects/11000744/matesoos/cryptominisat5-predict-main
-scp ../scripts/output_parser/concat_files.py ../scripts/output_parser/solved_with_options.sh ../scripts/output_parser/solvetimes_from_output.sh nscc://home/projects/11000744/matesoos/cryptominisat5-main/
+scp ../scripts/output_parser/create_solvetimes.sh ../scripts/output_parser/concat_files.py ../scripts/output_parser/solved_with_options.sh ../scripts/output_parser/solvetimes_from_output.sh nscc://home/projects/11000744/matesoos/cryptominisat5-main/
