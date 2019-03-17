7126a3b9d1d3ab497657211962884b399adb409a
Mate Soos <soos.mate@gmail.com>
2019-03-17 21:18:22 +0100

    Don't test static builds

--- build_static.sh
+++ build_static.sh
@@ -5,5 +5,4 @@ set -e
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 cmake -DENABLE_TESTING=ON -DREQUIRE_M4RI=ON -DSTATICCOMPILE=ON ..
 make -j6 VERBOSE=1
-make test
 strip cryptominisat5
