0068953c503818cee570011c9c8c49bfa25c0f4f
Mate Soos <soos.mate@gmail.com>
2019-05-01 15:02:57 +0200

    Bosphorus build

--- build_norm.sh
+++ build_norm.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc*
-cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DNOBOSPHORUS=OFF -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j6
 make test
