da4fb01681c9e72f61253a7ff602678ecce96bd3
Mate Soos <soos.mate@gmail.com>
2019-06-11 23:29:25 +0200

    Better AFL build

--- build_afl.sh
+++ build_afl.sh
@@ -5,4 +5,20 @@ set -e
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 CC=afl-clang-fast CXX=afl-clang-fast++ cmake -DLIMITMEM=ON -DENABLE_PYTHON_INTERFACE=OFF -DENABLE_TESTING=OFF -DNOZLIB=ON ..
 make -j6
+mkdir -p afl/testcase_dir/
+mkdir -p afl/findings_dir/
+cat <<EOF > afl/testcase_dir/input.cnf
+p cnf 13 24
+4 5 0
+-2 1 0
+c stuff
+c Solver::new_var()
+c Solver::new_vars( 20 )
+c Solver::solve( 3 4 )
+c Solver::simplify( 1 -3 )
+c Solver::solve( 1 -3 )
+2 3 0
+EOF
+
+export AFL_SKIP_CPUFREQ="1"
 afl-fuzz -M master -i afl/testcase_dir/ -o afl/findings_dir/ ./cryptominisat5 --debuglib 1
