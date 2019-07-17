4e7bafaf6730d4f81220ee02df822bb2bdee7bda
Mate Soos <soos.mate@gmail.com>
2019-07-17 22:03:57 +0800

    Better build script

--- build_norm.sh
+++ build_norm.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc*
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils drat-trim
 cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j6
 make test
