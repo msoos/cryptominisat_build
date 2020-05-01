58b23d4954e6bd72ca3be448ec731631ea855df2
Mate Soos <soos.mate@gmail.com>
2020-05-01 12:36:52 +0200

    No need for gauss builds

--- build_gauss_install_usr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include scalmc* cusp*
-rm -rf tests
-cmake -DUSE_GAUSS=ON -DENABLE_TESTING=ON -DCMAKE_INSTALL_PREFIX=/usr ..
-make -j6
-make test
--- build_gauss_sanitize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include scalmc* cusp*
-rm -rf tests
-CXX=clang++ cmake -DUSE_GAUSS=ON -DENABLE_TESTING=ON -DSANITIZE=ON ..
-make -j6
-make test
