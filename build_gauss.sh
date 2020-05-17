a2447b3f0a274b6ecf6f2baea1785eb80f148673
Mate Soos <soos.mate@gmail.com>
2020-05-17 12:08:55 +0200

    No breakid for gauss in for-trevor-xor-detach

--- build_gauss.sh
+++ build_gauss.sh
@@ -4,6 +4,6 @@ set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include scalmc* cusp*
 rm -rf tests
-cmake -DUSE_GAUSS=ON -DENABLE_TESTING=ON ..
+cmake -DUSE_GAUSS=ON -DENABLE_TESTING=ON -DNOBREAKID=ON ..
 make -j6
 make test
