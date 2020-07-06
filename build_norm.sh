4100a86e21ab1c517375039cfe360ac432db611b
Mate Soos <soos.mate@gmail.com>
2020-07-06 17:01:10 +0200

    No bosphorus for now

--- build_norm.sh
+++ build_norm.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils drat-trim
-cmake -DNOBOSPHORUS=OFF -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DNOBOSPHORUS=ON -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j6
 make test
