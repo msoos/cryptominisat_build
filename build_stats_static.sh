030a0d7d435314168805d8fffcb02145ec3a145a
Mate Soos <soos.mate@gmail.com>
2020-11-28 15:17:25 +0100

    Adding stripping of the binary

--- build_stats_static.sh
+++ build_stats_static.sh
@@ -6,4 +6,5 @@ rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 rm -f ../tests/cnf-files/*sqlite
 cmake -DSTATS=ON -DENABLE_TESTING=ON -DSTATICCOMPILE=ON ..
 make -j6
+strip cryptominisat5
 make test
