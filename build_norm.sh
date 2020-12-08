5dadd105a9059c98358780caf1b962cccb760f6c
Mate Soos <soos.mate@gmail.com>
2020-12-08 23:38:13 +0100

    Do compile at 16x

--- build_norm.sh
+++ build_norm.sh
@@ -4,5 +4,5 @@ set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils drat-trim
 cmake -DNOBOSPHORUS=ON -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-make -j6
+make -j16
 make test
--- build_stats.sh
+++ build_stats.sh
@@ -5,5 +5,5 @@ set -e
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 rm -f ../tests/cnf-files/*sqlite
 cmake -DSTATS=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-make -j6
+make -j16
 make test
