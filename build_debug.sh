4b3c265353491d2e222693417c9eef628957172c
Mate Soos <soos.mate@gmail.com>
2021-01-05 19:59:45 +0100

    Faster build

--- build_debug.sh
+++ build_debug.sh
@@ -5,5 +5,5 @@ set -e
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include
 rm -rf tests
 cmake -DENABLE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-make -j6
+make -j26
 make test
