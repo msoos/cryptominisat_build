5320649cb4adcf4ee55dedc688854ee4685e3a88
Mate Soos <soos.mate@gmail.com>
2021-01-23 23:13:41 +0100

    Improving compile speed

--- build_clang.sh
+++ build_clang.sh
@@ -4,5 +4,5 @@ set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 CXX=clang++ cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON ..
-make -j6
+make -j26
 make test
