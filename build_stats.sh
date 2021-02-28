1415d3d57cf3ec8bf9ffbcd84dae3eaf57f91528
Mate Soos <soos.mate@gmail.com>
2021-02-28 23:37:47 +0100

    Better build scripts

--- build_stats.sh
+++ build_stats.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils drat-trim *.cmake
 rm -f ../tests/cnf-files/*sqlite
 cmake -DSTATS=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j16
