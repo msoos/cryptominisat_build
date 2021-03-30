31ae4ee30db629929764fa1a040d225a99bf6505
Mate Soos <soos.mate@gmail.com>
2021-03-30 22:41:02 +0200

    Updating build script

--- build_static.sh
+++ build_static.sh
@@ -4,5 +4,5 @@ set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 cmake -DENABLE_TESTING=ON -DREQUIRE_M4RI=ON -DSTATICCOMPILE=ON ..
-make -j6 VERBOSE=1
+make -j16 VERBOSE=1
 strip cryptominisat5
