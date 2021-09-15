df2b16e8bef07cfc103a116dfc250da68e348fd2
Mate Soos <soos.mate@gmail.com>
2021-09-15 17:49:28 +0200

    Disable BreakID for predict build

--- build_final_predictor_static.sh
+++ build_final_predictor_static.sh
@@ -4,6 +4,6 @@ set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 rm -f ../tests/cnf-files/*sqlite
-cmake -DFINAL_PREDICTOR=ON -DSTATICCOMPILE=ON ..
+cmake -DFINAL_PREDICTOR=ON -DSTATICCOMPILE=ON -DNOBREAKID=ON ..
 make -j6
 strip cryptominisat5
