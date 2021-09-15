905524c506aa4708597be01ca2ba717bad985c83
Mate Soos <soos.mate@gmail.com>
2021-09-15 17:51:26 +0200

    Faster better builds

--- build_final_predictor_static.sh
+++ build_final_predictor_static.sh
@@ -5,5 +5,5 @@ set -e
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
 rm -f ../tests/cnf-files/*sqlite
 cmake -DFINAL_PREDICTOR=ON -DSTATICCOMPILE=ON -DNOBREAKID=ON ..
-make -j6
+make -j26
 strip cryptominisat5
--- build_static.sh
+++ build_static.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
-cmake -DENABLE_TESTING=ON -DREQUIRE_M4RI=ON -DSTATICCOMPILE=ON ..
-make -j16 VERBOSE=1
+cmake -DENABLE_TESTING=ON -DREQUIRE_M4RI=ON -DSTATICCOMPILE=ON -DNOBREAKID=ON ..
+make -j26
 strip cryptominisat5
