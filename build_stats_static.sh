79881f176ec75c5934530cf36813550851fca412
Mate Soos <soos.mate@gmail.com>
2021-12-12 22:02:59 +0100

    Fixing build script

--- build_stats_static.sh
+++ build_stats_static.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests utils
 rm -f ../tests/cnf-files/*sqlite
 cmake -DSTATS=ON -DENABLE_TESTING=ON -DSTATICCOMPILE=ON ..
 make -j6
