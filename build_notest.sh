c281adfa3982acbc65b735e3556de62bd1264733
Mate Soos <soos.mate@gmail.com>
2023-05-04 20:55:20 -0700

    Fixing notest to be better

--- build_notest.sh
+++ build_notest.sh
@@ -3,5 +3,5 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests
-cmake -DENABLE_PYTHON_INTERFACE=ON ..
-make -j26
+cmake -DNOTBUDDY=OFF -DNOBREAKID=ON -DNOBOSPHORUS=ON -DENABLE_PYTHON_INTERFACE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+make -j12
