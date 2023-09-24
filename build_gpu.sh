458e5e12f060a281c887445083aaa0f4566f311e
Mate Soos <soos.mate@gmail.com>
2023-09-24 15:14:20 +0200

    Updating to no drat-trim

--- build_gpu.sh
+++ build_gpu.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils drat-trim *.cmake Make*
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils *.cmake Make*
 cmake -DNOBOSPHORUS=ON -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DGPU=ON ..
 make -j26
 make test
