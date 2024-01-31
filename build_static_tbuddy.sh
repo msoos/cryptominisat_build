1920e7e026c70dd5df68dfad0a1231250857e3de
Mate Soos <soos.mate@gmail.com>
2024-01-31 20:19:31 +0100

    No need for tbuddy stuff

--- build_static_tbuddy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils Make*
-cmake -DSTATICCOMPILE=ON -DFORCETBUDDY=ON -DNOBREAKID=ON -DNOBOSPHORUS=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-make -j26
-make test
