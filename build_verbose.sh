084af1f2b7b4b068ac19949d2e3c862a1ca24874
Mate Soos <soos.mate@gmail.com>
2024-08-10 19:32:54 +0200

    Update verbose build

--- build_verbose.sh
+++ build_verbose.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include
-rm -rf tests
-cmake -DENABLE_TESTING=ON ..
-make -j6 VERBOSE=1
-ctest -V
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils Make*
+cmake -DNOBREAKID=ON -DNOBOSPHORUS=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+make -j26 VERBOSE=1
+make test
