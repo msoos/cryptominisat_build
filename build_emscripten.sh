a180eed56366c5b5e4f8a29a9aa53f8233e2c32e
Mate Soos <soos.mate@gmail.com>
2025-03-30 12:36:25 +0200

    Fixing up emscripten

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -2,6 +2,8 @@
 
 set -e
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests utils Make*
-emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL ..
-emmake make -j12
+emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF -DPOLYS=OFF ..
+emmake make -j26
 emmake make install
+cp cryptominisat5.wasm ../html
+cp $EMINSTALL/bin/cryptominisat5.js ../html
