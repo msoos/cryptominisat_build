bd8cc6fc91a17ed197a9c8f712f300ec9530d66e
Mate Soos <soos.mate@gmail.com>
2025-04-19 19:51:31 +0200

    update

--- build_ipasir.sh
+++ build_ipasir.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils Make*
-cmake -DIPASIR=ON -DSTATICCOMPILE=OFF -DSTATS=OFF -DENABLE_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DIPASIR=ON -DSTATICCOMPILE=OFF -DSTATS=OFF -DENABLE_TESTING=OFF ..
 make -j26
 make test
