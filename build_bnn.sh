d5120e8e336129165df170018668eb79dffae1d4
Mate Soos <soos.mate@gmail.com>
2025-04-23 20:50:27 +0200

    Removing unused flags

--- build_bnn.sh
+++ build_bnn.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils Make*
-cmake -DENABLE_BNN=ON -DNOBREAKID=ON -DNOBOSPHORUS=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DENABLE_BNN=ON -DNOBREAKID=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j26
 ctest --rerun-failed --output-on-failure
--- build_gpu.sh
+++ build_gpu.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests cusp* scalmc* utils *.cmake Make*
-cmake -DNOBOSPHORUS=ON -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DGPU=ON ..
+cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DGPU=ON ..
 make -j26
 make test
